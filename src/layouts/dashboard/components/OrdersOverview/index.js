c54143968623e2f043cc46c8564d4e0d43222d05
Devangi Bhutiya <devangibhutiya@gmail.com>
2023-06-27 17:39:56 +0530

    complete follow design

--- src/layouts/dashboard/components/OrdersOverview/index.js
+++ src/layouts/dashboard/components/OrdersOverview/index.js
@@ -1,19 +1,3 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// @mui material components
 import React, { useContext, useState, useEffect } from "react";
 
 import Card from "@mui/material/Card";
@@ -22,6 +6,7 @@ import Grid from "@mui/material/Grid";
 import team1 from "assets/images/team-1.jpg";
 import MDAvatar from "components/MDAvatar";
 import axios from "axios";
+import { makeStyles } from "@mui/styles";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
@@ -31,7 +16,52 @@ import MDTypography from "components/MDTypography";
 import TimelineItem from "examples/Timeline/TimelineItem";
 import { MainContext } from "context";
 
+const useStyles = makeStyles((theme) => ({
+  card: {
+    height: "100%",
+    padding: theme.spacing(1),
+    backgroundColor: "#f5f8fa",
+  },
+  header: {
+    padding: theme.spacing(2),
+    display: "flex",
+    justifyContent: "space-between",
+    // marginBottom: theme.spacing(2),
+  },
+  followText: {
+    color: "#344767",
+    fontSize: "20px",
+    fontWeight: "bold",
+    cursor: "pointer",
+    "&:hover": {
+      textDecoration: "underline",
+    },
+  },
+  avatarContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: theme.spacing(2),
+  },
+  name: {
+    fontWeight: "bold",
+    // marginBottom: theme.spacing(0.5),
+  },
+  username: {
+    color: "#657786",
+  },
+  followButton: {
+    color: "#344767",
+    fontWeight: "bold",
+    cursor: "pointer",
+    "&:hover": {
+      textDecoration: "underline",
+    },
+  },
+}));
+
 const OrdersOverview = () => {
+  const classes = useStyles();
   const { mainstate, setMainstate } = useContext(MainContext);
   const getAllUserInfo = async () => {
     debugger;
@@ -60,12 +90,12 @@ const OrdersOverview = () => {
   }, []);
 
   return (
-    <Card sx={{ height: "100%" }}>
-      <MDBox pt={3} px={3} style={{ display: "flex", justifyContent: "space-between" }}>
-        <MDTypography variant="h6" style={{ color: "red" }} fontWeight="medium">
+    <Card className={classes.card}>
+      <MDBox className={classes.header}>
+        <MDTypography variant="h6" className={classes.followText}>
           Who to Follow?
         </MDTypography>
-        <MDTypography variant="h6" style={{ color: "red" }} fontWeight="medium">
+        <MDTypography variant="h6" className={classes.followText}>
           Show More
         </MDTypography>
       </MDBox>
@@ -74,16 +104,16 @@ const OrdersOverview = () => {
           <MDBox key={data._id}>
             <Grid
               container
-              spacing={0}
+              // spacing={0}
               direction="row"
               alignItems="center"
               justifyContent="center"
               textAlign="center"
-              m={2}
+              className={classes.avatarContainer}
               style={{
-                width: "91%",
+                width: "100%",
                 borderRadius: "10px",
-                boxShadow: "3px 3px 3px 3px grey",
+                boxShadow: "3px 3px 3px 2px grey",
               }}
             >
               <Grid
@@ -100,13 +130,13 @@ const OrdersOverview = () => {
                   <MDAvatar src={team1} alt="something here" shadow="md" />
                 </MDBox>
               </Grid>
-              <Grid item md={4} style={{ marginBottom: "10px" }}>
+              <Grid item md={4} className={classes.name}>
                 {data.firstName} {data.lastName}
                 <br />
                 <small style={{ color: "#a09699" }}>@{data.username}</small>
               </Grid>
               <Grid item md={4}>
-                <small style={{ color: "red", fontWeight: "bold" }}>Follow+</small>
+                <small className={classes.followButton}>Follow+</small>
               </Grid>
             </Grid>
           </MDBox>
